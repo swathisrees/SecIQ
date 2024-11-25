10c9cc823ad4472a5fb57eeb41aa03eb8a762acb
swathisrees <70190980+swathisrees@users.noreply.github.com>
2024-11-25 20:05:04 +0530

    init

--- SecIQ/SecIQ/settings.py
+++ SecIQ/SecIQ/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&z!=_*2-7b1=)v9jm$%oxz%$e)wbbg2bropl0kw7acfrn0g@2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
